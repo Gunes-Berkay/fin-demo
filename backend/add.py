a4d344c1621b7bdcf9361dc95bd85ca0e6520a93
Dimitri Kapov <kapovdimitri93@gmail.com>
2025-01-20 12:12:19 +0300

    database güncellendi

--- backend/add.py
+++ backend/add.py
@@ -1,14 +1,15 @@
 import sqlite3
 import requests
 import os
+import trading
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-db_path = os.path.join(BASE_DIR, "db.sqlite3") # Django veritabanı dosyanızın yolu
-conn = sqlite3.connect(db_path)
-cursor = conn.cursor()
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# db_path = os.path.join(BASE_DIR, "db.sqlite3") 
+# conn = sqlite3.connect(db_path)
+# cursor = conn.cursor()
 
-# CoinMarketCap API'den veri çekme
+
 API_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 API_KEY = "e58be2f2-404d-4d5a-893a-cb486e74024d"
 
@@ -19,34 +20,28 @@ def fetch_top_100_coins():
     }
     params = {
         "start": "1",
-        "limit": "100",
+        "limit": "5",
         "convert": "USD",
     }
     response = requests.get(API_URL, headers=headers, params=params)
     data = response.json()
-    return data["data"]
 
-# Veritabanına verileri ekleme
+    
+    coins_dict = {coin['symbol']+'USDT': "BINANCE" for coin in data['data']}
+    symbol_name_dict = {coin['symbol']+'USDT':coin['name'] for coin in data['data']}
+
+    return coins_dict, symbol_name_dict
+
+
 def insert_coins_into_db():
-    coins = fetch_top_100_coins()
-    for coin in coins:
-        paper_id = coin["id"]
-        name = coin["name"]
-        try:
-            # Veriyi tabloya ekle
-            cursor.execute(
-                "INSERT INTO wallet_paper (paper_id, name) VALUES (?, ?)",
-                (paper_id, name),
-            )
-        except sqlite3.IntegrityError:
-            print(f"'{name}' zaten eklenmiş, atlanıyor...")
-    conn.commit()
-    print("İlk 100 coin başarıyla veritabanına eklendi.")
+    coins_dict, symbol_name_dict = fetch_top_100_coins()
+    trading.saveToDatabase(coins_dict, symbol_name_dict)
+    
 
-# Scripti çalıştırma
+
 if __name__ == "__main__":
     print()
     insert_coins_into_db()
 
 # Bağlantıyı kapat
-conn.close()
+# conn.close()
