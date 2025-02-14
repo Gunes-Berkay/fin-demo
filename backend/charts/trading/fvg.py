83321c1f4deef64e81c1bb1ddfb18bcde71f87dd
Dimitri Kapov <kapovdimitri93@gmail.com>
2025-02-14 23:11:57 +0300

    fvg güncellendi

--- backend/charts/trading/fvg.py
+++ backend/charts/trading/fvg.py
@@ -22,7 +22,7 @@ def find_fvg_levels(db_path, symbol, interval):
     bullish_fvg = []
     bearish_fvg = []
     percantage = 1
-    filled = 0
+    
 
     for i in range(1, len(df) - 1):
         prev_high = df.iloc[i - 1]['high']
@@ -32,11 +32,11 @@ def find_fvg_levels(db_path, symbol, interval):
         
         if (prev_high*(100+percantage)/100) < next_low:
             direction = "up"          
-            bullish_fvg.append((df.iloc[i]['datetime'], prev_high, next_low, direction, filled))
+            bullish_fvg.append((df.iloc[i]['datetime'], prev_high, next_low, direction, 0,0,0))
         
         elif (prev_low > (next_high*(percantage+100)/100)):
             direction = "bottom"            
-            bearish_fvg.append((df.iloc[i]['datetime'], next_high, prev_low, direction, filled))
+            bearish_fvg.append((df.iloc[i]['datetime'], next_high, prev_low, direction, 0,0,0))
     
     fvg_table = f"{symbol}on{interval}_fvg"
     cursor.execute(f"""
@@ -45,15 +45,17 @@ def find_fvg_levels(db_path, symbol, interval):
         start_price REAL,
         end_price REAL,
         direction TEXT,
-        filled BOOLEAN
+        touch_start BOOLEAN,
+        touch_half BOOLEAN,
+        touch_end BOOLEAN
     )""")
     
-    cursor.executemany(f"INSERT INTO {fvg_table} (datetime, start_price, end_price, direction, filled) VALUES (?, ?, ?, ?, ?)", bullish_fvg)
-    cursor.executemany(f"INSERT INTO {fvg_table} (datetime, start_price, end_price, direction, filled) VALUES (?, ?, ?, ?, ?)", bearish_fvg)
+    cursor.executemany(f"INSERT INTO {fvg_table} (datetime, start_price, end_price, direction, touch_start, touch_half, touch_end) VALUES (?, ?, ?, ?, ?, ?, ?)", bullish_fvg)
+    cursor.executemany(f"INSERT INTO {fvg_table} (datetime, start_price, end_price, direction, touch_start, touch_half, touch_end) VALUES (?, ?, ?, ?, ?, ?, ?)", bearish_fvg)
 
     conn.commit()
     conn.close()
     print(f" FVG seviyeleri {fvg_table} tablosuna kaydedildi.")
 
 
-find_fvg_levels(db_path, "BTCUSDT", "4h")
+find_fvg_levels(db_path, "DOGEUSDT", "4h")
